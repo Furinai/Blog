3114edb85756c993a60e8434eec34d76f1866f9c
wangxiaoyang <15847424609@163.com>
2020-07-15 23:25:18 +0800

    配置LocalDateTime格式化

---
+++ src/main/java/cn/linter/blog/config/LocalDateTimeFormatConfig.java
@@ -0,0 +1,28 @@
+package cn.linter.blog.config;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class LocalDateTimeFormatConfig {
+
+    @Value("${spring.jackson.date-format}")
+    private String pattern;
+
+    @Bean
+    public LocalDateTimeSerializer localDateTimeDeserializer() {
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer());
+    }
+
+}
