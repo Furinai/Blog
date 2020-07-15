5ec73f9f9c8dd16610936059f5df35c7b9bc38d7
wangxiaoyang <15847424609@163.com>
2020-07-15 23:26:34 +0800

    修改PasswordEncoder配置方式

---
+++ src/main/java/cn/linter/blog/config/PasswordEncoderConfig.java
@@ -0,0 +1,13 @@
+package cn.linter.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
