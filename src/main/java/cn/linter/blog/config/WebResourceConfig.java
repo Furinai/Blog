ccf35d892bae59de2a7b33e480483ee3fe633b3f
wangxiaoyang <15847424609@163.com>
2020-07-24 21:05:39 +0800

    静态资源映射改为实现WebMvcConfigurer接口

--- src/main/java/cn/linter/blog/config/WebResourceConfig.java
+++ src/main/java/cn/linter/blog/config/WebResourceConfig.java
@@ -3,16 +3,16 @@ package cn.linter.blog.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebResourceConfig extends WebMvcConfigurationSupport {
+public class WebResourceConfig implements WebMvcConfigurer {
 
     @Value("${blog.upload.image.location}")
     private String location;
 
     @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/image/**")
                 .addResourceLocations("file:" + location);
     }
